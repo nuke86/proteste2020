ab829bf444f0e9d33fbc9ba6df1304c19a4044f1
Nuke <nuke@spcnet.it>
2020-08-29 00:29:00 +0200

    port

--- src/main.js
+++ src/main.js
@@ -4,4 +4,9 @@ var app = new App({
 	target: document.body
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`App is running on port ${ PORT }`);
+});
+
 export default app;
